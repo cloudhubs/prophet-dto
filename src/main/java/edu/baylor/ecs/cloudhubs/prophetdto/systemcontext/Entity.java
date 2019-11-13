a947e2489ea58565683b498c50851330950f628a
Ian Laird <ian.c.laird@hotmail.com>
2019-11-13 13:45:16 -0600

    fixed no entity default constructor

--- src/main/java/edu/baylor/ecs/cloudhubs/prophetdto/systemcontext/Entity.java
+++ src/main/java/edu/baylor/ecs/cloudhubs/prophetdto/systemcontext/Entity.java
@@ -6,6 +6,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 
+@NoArgsConstructor
 public class Entity {
 
     @NonNull
--- src/main/java/edu/baylor/ecs/cloudhubs/prophetdto/systemcontext/Module.java
+++ src/main/java/edu/baylor/ecs/cloudhubs/prophetdto/systemcontext/Module.java
@@ -5,6 +5,7 @@ import lombok.*;
 import java.util.List;
 import java.util.Objects;
 
+@NoArgsConstructor
 public class Module {
 
     @NonNull
