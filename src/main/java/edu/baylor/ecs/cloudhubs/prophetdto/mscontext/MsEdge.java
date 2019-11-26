165dfcf773a1302db1c1d6dcfd70f61a1191e860
Dipta Das <dipta670@gmail.com>
2019-11-26 17:05:31 -0600

    Associate MsNode with MsEdge

--- src/main/java/edu/baylor/ecs/cloudhubs/prophetdto/mscontext/MsEdge.java
+++ src/main/java/edu/baylor/ecs/cloudhubs/prophetdto/mscontext/MsEdge.java
@@ -4,14 +4,14 @@ import lombok.Data;
 
 @Data
 public class MsEdge {
-    private Integer from;
-    private Integer to;
+    private MsNode from;
+    private MsNode to;
     private Integer width = 1;
     private Integer length = 200;
     private MsLabel label;
 
     @Override
     public String toString() {
-        return String.format("{ from: %d, to: %d, width: %d, length: %d, label: %s }", from, to, width, length, label);
+        return String.format("{ from: %s, to: %s, width: %d, length: %d, label: %s }", from, to, width, length, label);
     }
 }
--- src/main/java/edu/baylor/ecs/cloudhubs/prophetdto/mscontext/MsNode.java
+++ src/main/java/edu/baylor/ecs/cloudhubs/prophetdto/mscontext/MsNode.java
@@ -1,12 +1,18 @@
 package edu.baylor.ecs.cloudhubs.prophetdto.mscontext;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class MsNode {
     private Integer id;
     private String label;
     private String shape = "box";
+
+    public MsNode(String label) {
+        this.label = label;
+    }
 
     @Override
     public String toString() {
