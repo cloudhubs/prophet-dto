0109acc49e4ed50861e74760450e2249c9b7658a
Jan Svacina <jan@svacina.net>
2019-12-30 23:19:50 +0100

    prophet request upgrade

--- src/main/java/edu/baylor/ecs/cloudhubs/prophetdto/app/ProphetRequest.java
+++ src/main/java/edu/baylor/ecs/cloudhubs/prophetdto/app/ProphetRequest.java
@@ -1,10 +1,16 @@
 package edu.baylor.ecs.cloudhubs.prophetdto.app;
 
+import java.util.Objects;
+
 public class ProphetRequest {
 
-    private String url;
+    String url;
 
     public ProphetRequest(){}
+
+    public ProphetRequest(String url) {
+        this.url = url;
+    }
 
     public String getUrl() {
         return url;
@@ -12,5 +18,25 @@ public class ProphetRequest {
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ProphetRequest)) return false;
+        ProphetRequest that = (ProphetRequest) o;
+        return Objects.equals(getUrl(), that.getUrl());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUrl());
+    }
+
+    @Override
+    public String toString() {
+        return "ProphetRequest{" +
+                "url='" + url + '\'' +
+                '}';
     }
 }
