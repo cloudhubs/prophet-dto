4d1d2acab7b5b2ff4c267bc8a5800d776ae226a5
Vincent Bushong <vinbush@gmail.com>
2020-01-22 10:49:22 -0600

    Added classes for Prophet 'app' endpoint

--- src/main/java/edu/baylor/ecs/cloudhubs/prophetdto/app/ProphetAppData.java
+++ src/main/java/edu/baylor/ecs/cloudhubs/prophetdto/app/ProphetAppData.java
@@ -1,5 +1,24 @@
 package edu.baylor.ecs.cloudhubs.prophetdto.app;
 
-public class ProphetAppData {
+import edu.baylor.ecs.cloudhubs.prophetdto.communication.Communication;
+import edu.baylor.ecs.cloudhubs.prophetdto.communication.ContextMap;
 
+import java.util.List;
+
+public class ProphetAppData {
+    private String projectName;
+    private ContextMap communication;
+    private ContextMap contextMap;
+    private List<MicroserviceResult> microservices;
+
+    public ProphetAppData() {}
+
+    public String getProjectName() { return projectName; }
+    public void setProjectName(String projectName) { this.projectName = projectName; }
+    public ContextMap getCommunication() { return communication; }
+    public void setCommunication(ContextMap communication) { this.communication = communication; }
+    public ContextMap getContextMap() { return contextMap; }
+    public void setContextMap(ContextMap contextMap) { this.contextMap = contextMap; }
+    public List<MicroserviceResult> getMicroservices() { return microservices; }
+    public void setMicroservices(List<MicroserviceResult> microservices) { this.microservices = microservices; }
 }
