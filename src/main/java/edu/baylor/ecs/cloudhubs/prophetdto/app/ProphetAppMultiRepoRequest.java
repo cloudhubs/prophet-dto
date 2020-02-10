ba3a2eaa24a0175efa42684c220104b2f2d6ae60
Vincent Bushong <vinbush@gmail.com>
2020-02-10 11:14:29 -0600

    modified multi-repo request

--- src/main/java/edu/baylor/ecs/cloudhubs/prophetdto/app/ProphetAppMultiRepoRequest.java
+++ src/main/java/edu/baylor/ecs/cloudhubs/prophetdto/app/ProphetAppMultiRepoRequest.java
@@ -3,34 +3,30 @@ package edu.baylor.ecs.cloudhubs.prophetdto.app;
 import java.util.List;
 
 public class ProphetAppMultiRepoRequest {
-    private List<String> paths;
-    private boolean cached;
-    private boolean persistDb;
-    private boolean isMonolith;
-    private boolean all;
-    private boolean communication;
+    List<ProphetAppRequest> repositories;
+    String msSystemName;
 
-    public ProphetAppMultiRepoRequest() {}
-
-    public ProphetAppMultiRepoRequest(List<String> paths, boolean cached, boolean persistDb, boolean isMonolith, boolean all, boolean communication) {
-        this.paths = paths;
-        this.cached = cached;
-        this.persistDb = persistDb;
-        this.isMonolith = isMonolith;
-        this.all = all;
-        this.communication = communication;
+    public ProphetAppMultiRepoRequest() {
     }
 
-    public List<String> getPaths() { return paths; }
-    public void setPaths(List<String> paths) { this.paths = paths; }
-    public boolean isCached() { return cached; }
-    public void setCached(boolean cached) { this.cached = cached; }
-    public boolean isPersistDb() { return persistDb; }
-    public void setPersistDb(boolean persistDb) { this.persistDb = persistDb; }
-    public boolean isMonolith() { return isMonolith; }
-    public void setMonolith(boolean monolith) { isMonolith = monolith; }
-    public boolean isAll() { return all; }
-    public void setAll(boolean all) { this.all = all; }
-    public boolean isCommunication() { return communication; }
-    public void setCommunication(boolean communication) { this.communication = communication; }
+    public ProphetAppMultiRepoRequest(List<ProphetAppRequest> repositories, String msSystemName) {
+        this.repositories = repositories;
+        this.msSystemName = msSystemName;
+    }
+
+    public List<ProphetAppRequest> getRepositories() {
+        return repositories;
+    }
+
+    public void setRepositories(List<ProphetAppRequest> repositories) {
+        this.repositories = repositories;
+    }
+
+    public String getMsSystemName() {
+        return msSystemName;
+    }
+
+    public void setMsSystemName(String msSystemName) {
+        this.msSystemName = msSystemName;
+    }
 }
