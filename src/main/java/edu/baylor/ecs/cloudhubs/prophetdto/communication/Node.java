2406b6c1390436614bd9f555f4033e1f55f018fa
i-laird <ian.c.laird@hotmail.com>
2020-03-02 14:04:07 -0600

    added condition string to the edge in communication diagram

--- src/main/java/edu/baylor/ecs/cloudhubs/prophetdto/communication/Node.java
+++ src/main/java/edu/baylor/ecs/cloudhubs/prophetdto/communication/Node.java
@@ -1,8 +1,10 @@
 package edu.baylor.ecs.cloudhubs.prophetdto.communication;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Node {
 
     private String id;
