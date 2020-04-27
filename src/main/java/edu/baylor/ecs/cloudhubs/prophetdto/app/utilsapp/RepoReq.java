d1a5b0a68a394172c2c5fb8eb4dcc5d6774bb0dc
Vincent Bushong <vinbush@gmail.com>
2020-04-26 23:50:36 -0500

    Update to version 0.0.8; GitReq now just has path

--- src/main/java/edu/baylor/ecs/cloudhubs/prophetdto/app/utilsapp/RepoReq.java
+++ src/main/java/edu/baylor/ecs/cloudhubs/prophetdto/app/utilsapp/RepoReq.java
@@ -1,18 +1,12 @@
 package edu.baylor.ecs.cloudhubs.prophetdto.app.utilsapp;
 
 public class RepoReq {
-    private String localPath;
+    private String path;
     private boolean isMonolith;
-    private String organization;
-    private String repository;
     public RepoReq() {}
-    public String getLocalPath() {return localPath; }
-    public void setLocalPath(String localPath) { this.localPath = localPath; }
+    public String getPath() {return path; }
+    public void setPath(String path) { this.path = path; }
     public boolean isMonolith() { return isMonolith; }
     public void setMonolith(boolean monolith) { isMonolith = monolith; }
-    public String getOrganization() { return organization; }
-    public void setOrganization(String organization) { this.organization = organization; }
-    public String getRepository() { return repository; }
-    public void setRepository(String repository) { this.repository = repository; }
 }
 
